f58520ca5c783f9bc43c053b3e0b6ff4826f34b5
Jodwinn <pavelkharkovski@gmail.com>
2024-09-21 14:49:43 +0300

    Create Task2.cpp

---
+++ Task2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+int main() {
+	int a;
+	unsigned int b;
+	a = 21474836485;
+	b = 42949673009;
+	cout << a << endl ;
+	cout << b << endl ;
+
+}
